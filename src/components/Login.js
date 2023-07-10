cd45e4dc548f6a8c1bb64d09c327974e131b96fe
+ <+>
2023-07-10 11:41:56 +0300

    Update

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import backgroundImage from '../assets/hotdog03.jpg';
 
 function Login({ setCurrentUser }) {
   const [userLog, setUserLog] = useState("");
@@ -9,8 +8,6 @@ function Login({ setCurrentUser }) {
   const [userSign, setUserSign] = useState("");
   const [passSign, setPassSign] = useState("");
   const [errors, setErrors] = useState([]);
-  const [hasSignedUp, setHasSignedUp] = useState(false);
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
   const navigate = useNavigate();
 
   function onSignupSubmit(e) {
@@ -24,14 +21,13 @@ function Login({ setCurrentUser }) {
     })
       .then((res) => {
         if (res.ok) {
-          setHasSignedUp(true);
-          setIsLoggedIn(true);
+          res.json().then((user) => {
+            setCurrentUser(user);
+            navigate("/find");
+          });
         } else {
-          handleErrors(res);
+          res.json().then((errorData) => setErrors(errorData.errors));
         }
-      })
-      .catch((error) => {
-        console.error("Signup request failed:", error);
       });
   }
 
@@ -46,124 +42,97 @@ function Login({ setCurrentUser }) {
     })
       .then((res) => {
         if (res.ok) {
-          res.json().then((userData) => {
-            setCurrentUser(userData);
-            setIsLoggedIn(true);
+          res.json().then((user) => {
+            setCurrentUser(user);
             navigate("/find");
           });
         } else {
-          handleErrors(res);
+          res.json().then((errorData) => setErrors(errorData.errors));
         }
-      })
-      .catch((error) => {
-        console.error("Login request failed:", error);
       });
   }
 
-  function handleErrors(res) {
-    res.json().then((errorData) => {
-      if (errorData && errorData.errors) {
-        setErrors(errorData.errors);
-      } else {
-        console.error("Error data is invalid:", errorData);
-      }
-    });
-  }
+  const imageHoverEnter = (e) => {
+    e.target.src = "https://i.imgur.com/01iYgmo.png";
+  };
+
+  const imageHoverExit = (e) => {
+    e.target.src = "https://i.imgur.com/oAFz2Qm.png";
+  };
 
   return (
     <div>
-      <div style={{ backgroundImage: `url(${backgroundImage})` }} className="flex-container">
+      <img
+        className="welcome-header"
+        src="https://i.imgur.com/I4Xf9E8.jpg"
+        alt="welcome-header"
+      />
+      <div className="flex-container">
         <div className="welcome">
           <img
             className="welcome-img"
-            src="Cast.png"
+            src="https://i.imgur.com/oAFz2Qm.png"
             alt="welcome-img"
+            onMouseEnter={imageHoverEnter}
+            onMouseLeave={imageHoverExit}
           />
         </div>
+
         <div className="login-form-container">
-          {!isLoggedIn && !hasSignedUp && (
-            <div className="login-form-content-bottom">
-              <form className="sign-up-form" onSubmit={onSignupSubmit}>
-                <input
-                  className="form-inputs"
-                  placeholder="First and Last Name"
-                  type="text"
-                  value={nameSign}
-                  onChange={(e) => setNameSign(e.target.value)}
-                ></input>
-                <input
-                  className="form-inputs"
-                  placeholder="Username"
-                  type="text"
-                  value={userSign}
-                  onChange={(e) => setUserSign(e.target.value)}
-                ></input>
-                <input
-                  className="form-inputs"
-                  placeholder="Password"
-                  type="password"
-                  value={passSign}
-                  onChange={(e) => setPassSign(e.target.value)}
-                ></input>
-                <button className="form-input-btn" type="submit">
-                  Signup
-                </button>
-              </form>
-            </div>
-          )}
+          <div className="login-form-content-top">
+            <form className="login-form" onSubmit={onLoginSubmit}>
+              <input
+                className="form-inputs"
+                placeholder=" Username"
+                type="text"
+                value={userLog}
+                onChange={(e) => setUserLog(e.target.value)}
+              ></input>
+              <input
+                className="form-inputs"
+                placeholder=" Password"
+                type="password"
+                value={passLog}
+                onChange={(e) => setPassLog(e.target.value)}
+              ></input>
+              <button className="form-input-btn" type="submit">
+                Login
+              </button>
+            </form>
+          </div>
 
-          {!isLoggedIn && (
-            <div className="login-form-content-top">
-              <form className="login-form" onSubmit={onLoginSubmit}>
-                <input
-                  className="form-inputs"
-                  placeholder="Username"
-                  type="text"
-                  value={userLog}
-                  onChange={(e) => setUserLog(e.target.value)}
-                ></input>
-                <input
-                  className="form-inputs"
-                  placeholder="Password"
-                  type="password"
-                  value={passLog}
-                  onChange={(e) => setPassLog(e.target.value)}
-                ></input>
-                <button className="form-input-btn" type="submit">
-                  Login
-                </button>
-              </form>
-            </div>
-          )}
-
-          {isLoggedIn && (
-            <div className="login-form-content-top">
-              <h2>Signup Successful!</h2>
-
-              <form className="login-form" onSubmit={onLoginSubmit}>
-                <input
-                  className="form-inputs"
-                  placeholder="Username"
-                  type="text"
-                  value={userLog}
-                  onChange={(e) => setUserLog(e.target.value)}
-                ></input>
-                <input
-                  className="form-inputs"
-                  placeholder="Password"
-                  type="password"
-                  value={passLog}
-                  onChange={(e) => setPassLog(e.target.value)}
-                ></input>
-                <button className="form-input-btn" type="submit">
-                  Login
-                </button>
-              </form>
-            </div>
-          )}
-
+          <div className="login-form-content-bottom">
+            <form className="sign-up-form" onSubmit={onSignupSubmit}>
+              <input
+                className="form-inputs"
+                placeholder=" First and Last Name"
+                type="text"
+                value={nameSign}
+                onChange={(e) => setNameSign(e.target.value)}
+              ></input>
+              <input
+                className="form-inputs"
+                placeholder=" Username"
+                type="text"
+                value={userSign}
+                onChange={(e) => setUserSign(e.target.value)}
+              ></input>
+              <input
+                className="form-inputs"
+                placeholder=" Password"
+                type="password"
+                value={passSign}
+                onChange={(e) => setPassSign(e.target.value)}
+              ></input>
+              <button className="form-input-btn" type="submit">
+                Signup
+              </button>
+            </form>
+          </div>
           <div className="error-message">
-            {errors && errors.map((error) => <p key={error}>{error}</p>)}
+            {errors
+              ? errors.map((error) => <p key={error}>{error}</p>)
+              : null}
           </div>
         </div>
       </div>
